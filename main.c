d6734c4bdcb9f015c0db95eb286b1dd22ca556a7
Gizem Celik <gzmck237884@gmail.com>
2026-01-05 20:39:17 +0300

    added read line function

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LSH_RL_BUFSIZE 1024
+#define LSH_BUFFER
+char **lsh_split_line(char *line)
+{
+}
+
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+    if (!buffer)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    while (1)
+    {
+        c = getchar();
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
+                fprintf(stderr, "lsh: reallocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+void lsh_loop()
+{
+    char *line;
+    char **args;
+    int status;
+    do
+    {
+        printf("myShell> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+        free(line);
+        free(args);
+
+    } while (status);
+}
+
+int main(int argc, char **argv)
+{
+    lsh_loop();
+
+    return 0;
+}
